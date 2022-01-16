bb79ac9f06aac036a6daa9fd11199a4e5bd1546c
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-16 14:30:21 +0400

    first commit

---
+++ weshouldsplit/weshouldsplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  weshouldsplitApp.swift
+//  weshouldsplit
+//
+//  Created by Irakli Sokhaneishvili on 16.01.22.
+//
+
+import SwiftUI
+
+@main
+struct weshouldsplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
