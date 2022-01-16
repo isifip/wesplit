52dfb23da0c25fce9d873c4e082293cf60d5532d
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-16 14:48:44 +0400

    - Add background - Custom buttons

--- weshouldsplit/ContentView.swift
+++ weshouldsplit/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .top) {
+            Image("background")
+                .resizable()
+                .scaleEffect(2)
+            VStack {
+                HStack {
+                    Button {
+                        // more code to come
+                    } label: {
+                        ButtonLabelView(systemName: "paintbrush")
+                    }
+                    .padding(.leading)
+                    Spacer()
+                    Button {
+                        // more code to come
+                    } label: {
+                        ButtonLabelView(systemName: "arrow.counterclockwise")
+                    }
+                    .padding(.trailing)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct ButtonLabelView: View {
+    var systemName: String
+    var body: some View {
+        Image(systemName: systemName)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 20, height: 20)
+            .padding()
+            .background(.ultraThinMaterial)
+            .clipShape(Circle())
+            .shadow(radius: 20)
+            .foregroundColor(.primary)
     }
 }
